da25d5cd85f57541426c6d0519312e9880c6442a
simokhounti <larbikhounti@gmail.com>
2025-07-24 12:35:27 +0100

    Refactor signIn method in AuthController to use SignInDto for improved type safety

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
+import { SignInDto } from '../dtos/signin.dto';
 
 
 @Controller('auth')
@@ -9,7 +10,7 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.username, signInDto.password);
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
   }
 }
