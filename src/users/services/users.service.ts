5b3ffc00de12f4c0d55e2e8718f16854b3b3f3dd
simokhounti <larbikhounti@gmail.com>
2025-07-24 11:33:58 +0100

    Refactor UsersService to include create method for user registration

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterUserDto } from '../dtos/register.dto';
 
 @Injectable()
 export class UsersService {
     constructor(
         private readonly prisma: PrismaService
     ) {}
+
+    async create(data: RegisterUserDto): Promise<User> {
+        return this.prisma.user.create({
+            data,
+        });
+    }
+
     async findOne(email: string): Promise<User | null> {
     return this.prisma.user.findUnique({
       where: { email },
     });
   }
+
+
+
 }
