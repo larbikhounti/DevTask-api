60c98aee9193feda158495ef09283b49ddfb95ab
simokhounti <larbikhounti@gmail.com>
2025-07-24 12:21:01 +0100

    Add Exceptions provider to UsersModule for improved error handling

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserController } from './controllers/user.controller';
+import { Exceptions } from 'src/exceptions/exceptions.execptions';
 
 @Module({
-  providers: [UsersService, PrismaService],
+  providers: [UsersService, PrismaService, Exceptions],
   controllers: [UserController],
   exports: [UsersService],
 })
