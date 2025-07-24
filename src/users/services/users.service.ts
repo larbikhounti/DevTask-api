298a08cdc7d55ed89d8d87c60d156cd3c81d94d8
simokhounti <larbikhounti@gmail.com>
2025-07-24 12:21:19 +0100

    Refactor UsersService to integrate custom exception handling in user creation

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,26 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterUserDto } from '../dtos/register.dto';
+import { Exceptions } from 'src/exceptions/exceptions.execptions';
 
 @Injectable()
 export class UsersService {
     constructor(
-        private readonly prisma: PrismaService
-    ) {}
+        private readonly prisma: PrismaService,
+        private readonly exceptions: Exceptions
+    ) { }
 
-    async create(data: RegisterUserDto): Promise<User> {
-        return this.prisma.user.create({
-            data,
-        });
+    async create(data: RegisterUserDto): Promise<string | Error> {
+        try {
+            await this.prisma.user.create({
+                data,
+            });
+            return `User created successfully`;
+
+        } catch (error) {
+
+            return this.exceptions.ExceptionHandler(error.code);
+
+        }
     }
 
     async findOne(email: string): Promise<User | null> {
-    return this.prisma.user.findUnique({
-      where: { email },
-    });
-  }
-
-
-
+        try {
+            return await this.prisma.user.findUnique({
+                where: { email },
+            });
+        } catch (error) {
+            throw new Error('Error finding user');
+        }
+    }
 }
