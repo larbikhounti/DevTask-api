230cff0da650b24c52e0083d89f4fe91bda5ba4c
simokhounti <larbikhounti@gmail.com>
2025-07-24 12:25:25 +0100

    Add Helpers to UsersModule providers for enhanced functionality

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UsersService } from './services/users.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserController } from './controllers/user.controller';
 import { Exceptions } from 'src/exceptions/exceptions.execptions';
+import { Helpers } from 'src/helpers/helper.helpers';
 
 @Module({
-  providers: [UsersService, PrismaService, Exceptions],
+  providers: [UsersService, PrismaService, Exceptions, Helpers],
   controllers: [UserController],
   exports: [UsersService],
 })
