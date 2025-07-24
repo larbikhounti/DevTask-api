41c3eaf82e6a4c696da6411298a5caefdd6a10d9
simokhounti <larbikhounti@gmail.com>
2025-07-24 14:45:14 +0100

    Add Public decorator to registerUser endpoint and import AuthGuard in UsersModule

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -1,15 +1,18 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from "@nestjs/common";
 import { UsersService } from "../services/users.service";
 import { RegisterUserDto } from "../dtos/register.dto";
+import { Public } from "src/auth/decorator/public.decorator";
 
 @Controller('users')
 export class UserController {
   constructor(private usersService: UsersService) {}
 
   // register user
+  @Public()
   @HttpCode(HttpStatus.CREATED)
   @Post('register')
   registerUser(@Body() registerUserDto: RegisterUserDto) {
     return this.usersService.create(registerUserDto);
-    }
+  }
+
 }
