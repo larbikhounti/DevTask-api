61f3f9ba4fafe997d535090ee09c13607ada434d
simokhounti <larbikhounti@gmail.com>
2025-07-24 12:35:33 +0100

    Refactor signIn method in AuthService to use SignInDto and Helpers for improved validation and security

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -3,25 +3,34 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/services/users.service';
+import { SignInDto } from '../dtos/signin.dto';
+import { Helpers } from 'src/helpers/helper.helpers';
+import { SignInResponseDto } from '../dtos/signinResponse.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private readonly helpers: Helpers, // Assuming Helpers is used for some utility functions
   ) {}
 
-  async signIn(
-    email: string,
-    pass: string,
-  ): Promise<{ access_token: string }> {
-    const user = await this.usersService.findOne(email);
-    if (user?.password !== pass) {
+  async signIn(signInDto: SignInDto): Promise<SignInResponseDto> {
+
+    const user = await this.usersService.findOne(signInDto.email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    if (!this.helpers.comparePassword(signInDto.password, user.password)) {
       throw new UnauthorizedException();
     }
+
     // sub is the subject of the JWT, typically the user ID
     const payload = { sub: user.id, email: user.email };
     return {
+      email: user?.email,
       access_token: await this.jwtService.signAsync(payload),
     };
   }
