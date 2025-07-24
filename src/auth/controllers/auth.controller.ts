42b89790c125f871e13894ef91be86e2c04f759b
simokhounti <larbikhounti@gmail.com>
2025-07-24 14:45:01 +0100

    Add Public decorator to AuthController for public access to login endpoint

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -2,12 +2,14 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { SignInRequestDto } from '../dtos/auth.dto';
+import { Public } from '../decorator/public.decorator';
 
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: SignInRequestDto) {
