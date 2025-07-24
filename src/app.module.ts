da62907999fe22103a3a4ee8e912f9a4f44466c9
simokhounti <larbikhounti@gmail.com>
2025-07-24 12:52:08 +0100

    Add global configuration setup in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ExceptionModule } from './exceptions/exceptions.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UsersModule, AuthModule, ExceptionModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }), 
+    UsersModule, 
+    AuthModule, 
+    ExceptionModule
+  ],
   controllers: [],
   providers: [],
 })
