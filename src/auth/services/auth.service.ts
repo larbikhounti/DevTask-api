d9edf08f9902199dc67b267e9f3e17d793af73f9
simokhounti <larbikhounti@gmail.com>
2025-07-24 18:56:39 +0100

    Refactor signIn method to improve refresh token handling and add refreshToken method for generating new access tokens

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 
-import { Injectable,  UnauthorizedException} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/services/users.service';
@@ -8,6 +8,7 @@ import { Helpers } from 'src/helpers/helper.helpers';
 import { User } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
 import { Response } from 'express';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
@@ -16,35 +17,77 @@ export class AuthService {
     private jwtService: JwtService,
     private readonly helpers: Helpers, // Assuming Helpers is used for some utility functions
     private readonly configService: ConfigService, // ConfigService to access environment variables
-  ) {}
+    private readonly prisma: PrismaService, // Inject PrismaService
+  ) { }
 
   async signIn(signInDto: SignInRequestDto, response: Response): Promise<SignInResponseDto> {
 
-    const user  = await this.usersService.findOne(signInDto.email);
+    const user = await this.usersService.findOne(signInDto.email);
 
     if (!signInDto.email || !signInDto.password || !user) {
       throw new UnauthorizedException('Email and password are required');
     }
 
     if (! await this.helpers.comparePassword(signInDto.password, user.password)) {
-     // this.logger.warn(`Failed login attempt with invalid password for email: ${signInDto.email}`);
+      // this.logger.warn(`Failed login attempt with invalid password for email: ${signInDto.email}`);
       throw new UnauthorizedException();
     }
 
-   const { accessToken, refreshToken } = await this.generateTokens(user);
-   response.cookie('refreshToken', refreshToken, {
-          httpOnly: true,
-          secure: true,
-          maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-          sameSite: 'strict',
-        });
-    
+ 
+    const { accessToken, refreshToken } = await this.generateTokens(user);
+
+    // update user refresh token
+    await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        refreshToken: await this.helpers.hashPassword(refreshToken), // Assuming you want to hash the password for the refresh token
+      },
+    });
+
+    response.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      sameSite: 'strict',
+    });
+
 
     return {
       email: user.email,
       name: user.name,
       access_token: accessToken,
     };
+  }
+
+
+  // refreshToken method to generate new access token using refresh token
+  async refreshToken(refreshToken: string): Promise<{ accessToken: string }> {
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token is required');
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(refreshToken, {
+        secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+      });
+
+      const user = await this.usersService.findOne(payload.email);
+      if (!user) {
+        throw new UnauthorizedException('User not found');
+      }
+      if (!await this.helpers.comparePassword(refreshToken, user.refreshToken)) {
+        // this.logger.warn(`Failed login attempt with invalid password for email: ${signInDto.email}`);
+        throw new UnauthorizedException();
+      }
+
+      const accessToken = await this.jwtService.signAsync(
+        { sub: user.id, email: user.email },
+        { secret: this.configService.get<string>('JWT_ACCESS_SECRET'), expiresIn: '15m' },
+      );
+
+      return { accessToken };
+    } catch (error) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
   }
 
   // auth.service.ts
