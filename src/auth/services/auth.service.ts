c5edc3448d7584242e3efba403d92bee7e45fabd
simokhounti <larbikhounti@gmail.com>
2025-07-24 18:43:06 +0100

    Refactor signIn method in AuthService to include response object and set refresh token cookie for improved session management

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,10 +1,13 @@
 
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { Injectable,  UnauthorizedException} from '@nestjs/common';
 
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/services/users.service';
 import { SignInRequestDto, SignInResponseDto } from '../dtos/auth.dto';
 import { Helpers } from 'src/helpers/helper.helpers';
+import { User } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
@@ -12,9 +15,10 @@ export class AuthService {
     private usersService: UsersService,
     private jwtService: JwtService,
     private readonly helpers: Helpers, // Assuming Helpers is used for some utility functions
+    private readonly configService: ConfigService, // ConfigService to access environment variables
   ) {}
 
-  async signIn(signInDto: SignInRequestDto): Promise<SignInResponseDto> {
+  async signIn(signInDto: SignInRequestDto, response: Response): Promise<SignInResponseDto> {
 
     const user  = await this.usersService.findOne(signInDto.email);
 
@@ -27,13 +31,37 @@ export class AuthService {
       throw new UnauthorizedException();
     }
 
-    // sub is the subject of the JWT, typically the user ID
-    const payload = { sub: user.id, email: user.email };
-    return {
+   const { accessToken, refreshToken } = await this.generateTokens(user);
+   response.cookie('refreshToken', refreshToken, {
+          httpOnly: true,
+          secure: true,
+          maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+          sameSite: 'strict',
+        });
+    
 
+    return {
       email: user.email,
       name: user.name,
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: accessToken,
     };
   }
+
+  // auth.service.ts
+  async generateTokens(user: User): Promise<{ accessToken: string; refreshToken: string }> {
+    const payload = { sub: user.id, email: user.email };
+
+    const accessToken = await this.jwtService.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+    });
+
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      expiresIn: '7d',
+      secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+    });
+
+    return { accessToken, refreshToken };
+  }
+
 }
