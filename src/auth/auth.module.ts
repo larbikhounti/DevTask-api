6581fc5e4fcbea1e86521fffd543a6a4e7ffe68b
simokhounti <larbikhounti@gmail.com>
2025-07-24 14:44:55 +0100

    Refactor AuthModule to include AuthGuard as a global provider and improve JWT sign options structure

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,27 +3,33 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { Helpers } from 'src/helpers/helper.helpers';
-
+import { AuthGuard } from './guards/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     UsersModule,
     ConfigModule,
     JwtModule.registerAsync({
-     // global: true,
+      // global: true,
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         secret: configService.get<string>('SECRET'),
-        expiresIn: configService.get<string>('JWT_EXPIRATION_TIME')
+        signOptions : {
+         expiresIn: configService.get<string>('JWT_EXPIRATION_TIME')
+        }
       }),
     }),
   ],
-  providers: [AuthService, Helpers],
+  providers: [AuthService, Helpers, AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },],
   controllers: [AuthController],
   exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
