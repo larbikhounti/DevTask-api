7a5f60b62a335660b052482d86b48dcd4a2c901f
simokhounti <larbikhounti@gmail.com>
2025-07-24 11:33:22 +0100

    Refactor AuthService to include JWT signing in signIn method and update parameter from username to email

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,20 +1,28 @@
 
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from 'src/users/services/users.service';
 
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/services/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
+  async signIn(
+    email: string,
+    pass: string,
+  ): Promise<{ access_token: string }> {
+    const user = await this.usersService.findOne(email);
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
-    const { password, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return result;
+    // sub is the subject of the JWT, typically the user ID
+    const payload = { sub: user.id, email: user.email };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
