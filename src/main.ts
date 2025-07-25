3f2672fcac2df6d17855253211e35e74ae188f86
simokhounti <larbikhounti@gmail.com>
2025-07-25 13:57:57 +0100

    Refactor main.ts to integrate Swagger documentation setup and improve code organization

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug'],
   });
-   app.useGlobalPipes(new ValidationPipe());
-   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('DevTask API')
+    .setDescription('API documentation for DevTask application')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
