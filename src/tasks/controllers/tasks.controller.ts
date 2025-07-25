4509b758fc6f815ed5de439bd7f4dad16bc8d06d
simokhounti <larbikhounti@gmail.com>
2025-07-25 16:06:07 +0100

    Add methods to update task priority, estimated time, and deadline in TasksController

--- src/tasks/controllers/tasks.controller.ts
+++ src/tasks/controllers/tasks.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Req, Query, Put, Patch } from '@nestjs/common';
 import { TasksService } from '../services/tasks.service';
 import { CreateTaskDto } from '../dto/create-task.dto';
-import { UpdateTaskDto } from '../dto/update-task.dto';
 import { Request } from 'express';
+import { Priority } from '@prisma/client';
+import { UpdateTaskDto } from '../dto/update-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -17,6 +18,24 @@ export class TasksController {
   @Get(':id/complete')
   complete(@Param('id') id: string, @Req() request: Request) {
     return this.tasksService.complete(+id, request);
+  }
+
+  // update priority
+  @Put(':id/priority')
+  updatePriority(@Query('id') id: string, @Query('priority') priority: Priority, @Req() request: Request) {
+    return this.tasksService.updatePriority(+id, priority, request);
+  }
+
+  // update estimated time
+  @Put(':id/estimated-time')
+  updateEstimatedTime(@Query('id') id: string, @Query('time') time: number, @Req() request: Request) {
+    return this.tasksService.updateEstimatedTime(+id, time, request);
+  }
+
+  // update deadline
+  @Put(':id/deadline')
+  updateDeadline(@Query('id') id: string, @Query('deadline') deadline: Date, @Req() request: Request) {
+    return this.tasksService.updateDeadline(+id, deadline, request);
   }
 
   @Get()
