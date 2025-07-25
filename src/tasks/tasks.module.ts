a1cd1823bc504ac1223b30c7a1c4092a85b35974
simokhounti <larbikhounti@gmail.com>
2025-07-25 14:24:00 +0100

    Add PrismaService to TasksModule providers for improved database access

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './services/tasks.service';
 import { TasksController } from './controllers/tasks.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, PrismaService],
+
 })
 export class TasksModule {}
