c38699f5b75c452ec1fcc999e4c8a333fb3faf13
simokhounti <larbikhounti@gmail.com>
2025-07-25 09:52:07 +0100

    Refactor refreshToken method to improve token verification and handling, and update user refresh token in cookies

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,5 +1,5 @@
 
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/services/users.service';
@@ -33,7 +33,7 @@ export class AuthService {
       throw new UnauthorizedException();
     }
 
- 
+
     const { accessToken, refreshToken } = await this.generateTokens(user);
 
     // update user refresh token
@@ -61,32 +61,41 @@ export class AuthService {
 
 
   // refreshToken method to generate new access token using refresh token
-  async refreshToken(refreshToken: string): Promise<{ accessToken: string }> {
+  async refreshToken(response: Response): Promise<{ accessToken: string }> {
+    // Extract refresh token from cookies
+    let refreshToken = response.req.cookies['refreshToken'];
     if (!refreshToken) {
       throw new UnauthorizedException('Refresh token is required');
     }
     try {
+      // Verify the refresh token
       const payload = await this.jwtService.verifyAsync(refreshToken, {
         secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
       });
-
+      // Find the user associated with the refresh token
       const user = await this.usersService.findOne(payload.email);
       if (!user) {
         throw new UnauthorizedException('User not found');
       }
+      // Check if the refresh token matches the one stored in the database
       if (!await this.helpers.comparePassword(refreshToken, user.refreshToken)) {
         // this.logger.warn(`Failed login attempt with invalid password for email: ${signInDto.email}`);
-        throw new UnauthorizedException();
+        throw new UnauthorizedException("Invalid refresh token");
       }
-
-      const accessToken = await this.jwtService.signAsync(
-        { sub: user.id, email: user.email },
-        { secret: this.configService.get<string>('JWT_ACCESS_SECRET'), expiresIn: '15m' },
-      );
+      // Generate new access token
+      const { accessToken, refreshToken: newRefreshToken } = await this.generateTokens(user);
+      
+      // Update the user's refresh token in the database
+      response.cookie('refreshToken', newRefreshToken, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+        sameSite: 'strict',
+      });
 
       return { accessToken };
     } catch (error) {
-      throw new UnauthorizedException('Invalid refresh token');
+      throw new HttpException('Invalid refresh token', HttpStatus.UNAUTHORIZED);
     }
   }
 
