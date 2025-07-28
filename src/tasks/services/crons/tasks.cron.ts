81c177123d1f2bc7dabec7678d239ff2c01d846c
simokhounti <larbikhounti@gmail.com>
2025-07-28 17:17:42 +0100

    Implement cron job to increment timer for enabled tasks every second

--- src/tasks/services/crons/tasks.cron.ts
+++ src/tasks/services/crons/tasks.cron.ts
@@ -1,8 +1,31 @@
 import { Cron } from "@nestjs/schedule";
+import { TasksService } from "../tasks.service";
+import { Injectable } from "@nestjs/common";
 
+@Injectable()
 export class TasksCron {
-  @Cron('0 0 * * *')
-  handleCron() {
-    console.log('Cron job executed');
-  }
+    constructor(
+        private readonly tasksService: TasksService,
+    ) { }
+
+    // This method will be executed every second
+    @Cron('*/1 * * * * *')
+    async handleCron() {
+        try {
+            const tasks = await this.tasksService.findEnabledTimerTasks();
+            if (!tasks || tasks.length === 0) {
+                console.log('No tasks with timer enabled found');
+                return;
+            }
+            // for each task, check if the current date is greater than the timer date
+            tasks.forEach(task => {
+                if (task.currentTimer && new Date(task.currentTimer) <= new Date()) {
+                    this.tasksService.incrementTimerPerSecond(task);
+                }
+            });
+        } catch (error) {
+            console.error('Error executing cron job:', error);
+        }
+    }
+
 }
