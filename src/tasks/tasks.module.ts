e847b37953a6767d5c88b97f559b304ca9632b05
simokhounti <larbikhounti@gmail.com>
2025-07-28 16:34:52 +0100

    Register TasksCron as a provider in TasksModule

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TasksService } from './services/tasks.service';
 import { TasksController } from './controllers/tasks.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { TasksCron } from './services/crons/tasks.cron';
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService, PrismaService],
+  providers: [TasksService, PrismaService, TasksCron],
 
 })
 export class TasksModule {}
