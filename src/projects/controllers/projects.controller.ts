c74dd5fa6ce5464bd2e1e12e403226c900c969b1
simokhounti <larbikhounti@gmail.com>
2025-07-30 16:52:47 +0100

    Refactor findAll method in ProjectsController to include clientId and request object

--- src/projects/controllers/projects.controller.ts
+++ src/projects/controllers/projects.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request, Query } from '@nestjs/common';
 import { ProjectsService } from '../services/projects.service';
 import { CreateProjectDto } from '../dto/create-project.dto';
 import { UpdateProjectDto } from '../dto/update-project.dto';
@@ -13,8 +13,8 @@ export class ProjectsController {
   }
 
   @Get()
-  findAll() {
-    return this.projectsService.findAll();
+  findAll(@Query('clientId') clientId: number, @Request() request: Request) {
+    return this.projectsService.findAll(clientId, request);
   }
 
   @Get(':id')
