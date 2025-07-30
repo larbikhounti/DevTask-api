f22fbcbe712391863ce75e0661b3526519a2e847
simokhounti <larbikhounti@gmail.com>
2025-07-30 17:20:56 +0100

    Refactor ProjectsModule to remove unused PrismaService and ClientsService

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './services/projects.service';
 import { ProjectsController } from './controllers/projects.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { ClientsService } from 'src/clients/services/clients.service';
+import { ClientsModule } from 'src/clients/clients.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
 
 @Module({
   controllers: [ProjectsController],
-  providers: [ProjectsService, PrismaService, ClientsService],
+  providers: [ProjectsService],
+  exports: [ProjectsService],
+  imports: [ClientsModule, PrismaModule],
 })
 export class ProjectsModule {}
