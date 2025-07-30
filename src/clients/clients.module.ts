063540cdd3289555c2f12ef075305acf28ecad7e
simokhounti <larbikhounti@gmail.com>
2025-07-30 12:04:02 +0100

    Add PrismaService to ClientsModule providers for database integration

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClientsController } from './controllers/clients.controller';
 import { ClientsService } from './services/clients.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 
 @Module({
   controllers: [ClientsController],
-  providers: [ClientsService],
+  providers: [ClientsService, PrismaService],
+  
 })
 export class ClientsModule {}
