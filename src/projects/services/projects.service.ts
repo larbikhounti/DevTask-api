e654138fef5527b204f13d99f37bb4955bc83264
simokhounti <larbikhounti@gmail.com>
2025-07-30 16:34:40 +0100

    Refactor ProjectsService to include request object in findOne and create methods

--- src/projects/services/projects.service.ts
+++ src/projects/services/projects.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateProjectDto } from '../dto/create-project.dto';
 import { UpdateProjectDto } from '../dto/update-project.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ClientsService } from 'src/clients/services/clients.service';
+import { JwtPayloadType } from 'src/auth/types/jwt-payload.type';
 
 @Injectable()
 export class ProjectsService {
@@ -14,7 +15,7 @@ export class ProjectsService {
   async create(createProjectDto: CreateProjectDto, request: Request) {
 
     try {
-      
+
       await this.clientsService.findOne(createProjectDto.clientId, request);
 
       return this.prismaService.projects.create({
@@ -37,13 +38,46 @@ export class ProjectsService {
 
   }
 
-  findAll() {
-    return `This action returns all projects`;
+ async findAll() {
+    
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} project`;
+async findOne(id: number, request: Request) {
+  try {
+    const { sub: userId } = request['user'] as JwtPayloadType;
+
+    const project = await this.prismaService.projects.findUnique({
+      where: { id,
+        client: {
+          userId: userId
+        }
+       },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        startDate: true,
+        endDate: true,
+        clientId: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    if (!project) {
+      throw new HttpException(`Project with ID ${id} not found`, HttpStatus.NOT_FOUND);
+    }
+
+
+    return project;
+
+  } catch (error) {
+    if (error instanceof HttpException) {
+      throw error;
+    }
+    throw new HttpException(`Error finding project with ID ${id}: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
   }
+}
 
   update(id: number, updateProjectDto: UpdateProjectDto) {
     return `This action updates a #${id} project`;
