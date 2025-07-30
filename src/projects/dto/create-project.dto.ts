01b2b89f9e3f24ed314d4a2d82ea3e565d1d9615
simokhounti <larbikhounti@gmail.com>
2025-07-30 12:18:54 +0100

    Add CreateProjectDto with validation and API documentation

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,50 @@
-export class CreateProjectDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDateString, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateProjectDto {
+    @ApiProperty({
+        description: 'The name of the project',
+        example: 'Project Alpha',
+        type: String,
+    })
+    @IsString()
+    name: string;
+
+    @ApiProperty({
+        description: 'A brief description of the project',
+        example: 'This project involves developing a new web application.',
+        type: String,
+        required: false,
+    })
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @ApiProperty({
+        description: 'The client ID associated with the project',
+        example: 1,
+        type: Number,
+    })
+    @IsNumber()
+    clientId: number;
+
+    @ApiProperty({
+        description: 'The estimated start date for the project',
+        example: '2023-01-01',
+        type: String,
+        required: false,
+    })
+    @IsOptional()
+    @IsDateString()
+    startDate?: Date;
+
+    @ApiProperty({
+        description: 'The estimated end date for the project',
+        example: '2023-12-31',
+        type: String,
+        required: false,
+    })
+    @IsOptional()
+    @IsDateString()
+    endDate?: Date;
+}
