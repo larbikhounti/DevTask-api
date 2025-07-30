9f41ebbb5cbcf0aa0b688cd6b65b441d5488da49
simokhounti <larbikhounti@gmail.com>
2025-07-30 12:04:34 +0100

    Fix import path for PrismaService in UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { UserController } from './controllers/user.controller';
 import { Exceptions } from 'src/exceptions/exceptions.execptions';
 import { Helpers } from 'src/helpers/helper.helpers';
-import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @Module({
   providers: [UsersService, PrismaService, Exceptions, Helpers],
