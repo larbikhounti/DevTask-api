a9d757325e5141d30c30d3ac0a6641bbbb8acff9
simokhounti <larbikhounti@gmail.com>
2025-07-30 12:18:58 +0100

    Implement project creation logic in ProjectsService using Prisma

--- src/projects/services/projects.service.ts
+++ src/projects/services/projects.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProjectDto } from '../dto/create-project.dto';
 import { UpdateProjectDto } from '../dto/update-project.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProjectsService {
+
+  constructor(private readonly prismaService: PrismaService) {}
   create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
+    return this.prismaService.projects.create({
+      data: createProjectDto,
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        startDate: true,
+        endDate: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
   }
 
   findAll() {
