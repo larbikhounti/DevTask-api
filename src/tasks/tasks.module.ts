ef93df5a5f2e03eaafad89ca9b6ce35209255ba3
simokhounti <larbikhounti@gmail.com>
2025-07-30 12:04:12 +0100

    Fix import path for PrismaService in TasksModule

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './services/tasks.service';
 import { TasksController } from './controllers/tasks.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { TasksCron } from './services/crons/tasks.cron';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [TasksController],
