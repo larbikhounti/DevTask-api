5d9be7cc0b9e6a9efc3ca70af17ede0a812278a5
simokhounti <larbikhounti@gmail.com>
2025-07-30 11:36:17 +0100

    Enhance client creation to include user ID from request context

--- src/clients/services/clients.service.ts
+++ src/clients/services/clients.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Request } from '@nestjs/common';
 import { CreateClientDto } from '../dto/create-client.dto';
 import { UpdateClientDto } from '../dto/update-client.dto';
+import { PrismaClient } from '@prisma/client';
+
 
 @Injectable()
 export class ClientsService {
-  create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+  constructor(
+    private readonly prisma: PrismaClient,
+  ) {}
+
+  create(createClientDto: CreateClientDto, request: Request) {
+    try {
+      const userId = request['user'].id; // Assuming user ID is stored in request.user
+      return this.prisma.clients.create({
+        data: { ...createClientDto, userId },
+      });
+    } catch (error) {
+      // Handle error
+      throw new HttpException(`Failed to create client`, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   findAll() {
