aee99b11f504df6cf3f154005dbb3c1d074c1ddd
simokhounti <larbikhounti@gmail.com>
2025-07-30 16:20:23 +0100

    Refactor create method in ProjectsService to include client validation and request object

--- src/projects/services/projects.service.ts
+++ src/projects/services/projects.service.ts
@@ -2,24 +2,39 @@ import { Injectable } from '@nestjs/common';
 import { CreateProjectDto } from '../dto/create-project.dto';
 import { UpdateProjectDto } from '../dto/update-project.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ClientsService } from 'src/clients/services/clients.service';
 
 @Injectable()
 export class ProjectsService {
 
-  constructor(private readonly prismaService: PrismaService) {}
-  create(createProjectDto: CreateProjectDto) {
-    return this.prismaService.projects.create({
-      data: createProjectDto,
-      select: {
-        id: true,
-        name: true,
-        description: true,
-        startDate: true,
-        endDate: true,
-        createdAt: true,
-        updatedAt: true,
-      },
-    });
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly clientsService: ClientsService,
+  ) { }
+  async create(createProjectDto: CreateProjectDto, request: Request) {
+
+    try {
+      
+      await this.clientsService.findOne(createProjectDto.clientId, request);
+
+      return this.prismaService.projects.create({
+        data: createProjectDto,
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          startDate: true,
+          endDate: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+
+    } catch (error) {
+      throw new Error(`Error creating project: ${error.message}`);
+
+    }
+
   }
 
   findAll() {
