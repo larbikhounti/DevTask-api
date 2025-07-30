e5bce7cd5ca39104bd3c281722d4c32c73ee383f
simokhounti <larbikhounti@gmail.com>
2025-07-30 17:21:02 +0100

    Refactor TasksModule to remove PrismaService from providers and ensure proper module imports

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TasksService } from './services/tasks.service';
 import { TasksController } from './controllers/tasks.controller';
 import { TasksCron } from './services/crons/tasks.cron';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ProjectsModule } from 'src/projects/projects.module';
+
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService, PrismaService, TasksCron],
-
+  providers: [TasksService,TasksCron],
+  imports: [PrismaModule, ProjectsModule],
 })
 export class TasksModule {}
