137ffc9790c621746c854826f0ec4371e77f330c
simokhounti <larbikhounti@gmail.com>
2025-07-30 16:20:04 +0100

    Add ClientsService to ProjectsModule providers

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProjectsService } from './services/projects.service';
 import { ProjectsController } from './controllers/projects.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ClientsService } from 'src/clients/services/clients.service';
 
 @Module({
   controllers: [ProjectsController],
-  providers: [ProjectsService, PrismaService],
+  providers: [ProjectsService, PrismaService, ClientsService],
 })
 export class ProjectsModule {}
