96bb4a94d02f4faa42427f7b4cd74e5b6260c59b
simokhounti <larbikhounti@gmail.com>
2025-07-31 16:30:13 +0100

    Add Helpers to providers in TasksModule for utility functions

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,11 +4,13 @@ import { TasksController } from './controllers/tasks.controller';
 import { TasksCron } from './services/crons/tasks.cron';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ProjectsModule } from 'src/projects/projects.module';
+import { Helpers } from 'src/helpers/helper.helpers';
 
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService,TasksCron],
+  providers: [TasksService,TasksCron, Helpers],
   imports: [PrismaModule, ProjectsModule],
+  exports: [TasksService],
 })
 export class TasksModule {}
