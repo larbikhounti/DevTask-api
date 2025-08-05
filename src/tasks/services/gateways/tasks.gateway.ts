d086b6522b6a4cf12b8dae95033b89aa8e0f0a52
simokhounti <larbikhounti@gmail.com>
2025-08-05 10:41:24 +0100

    Refactor TasksGateway to inject CACHE_MANAGER and utilize caching for task timer emissions

--- src/tasks/services/gateways/tasks.gateway.ts
+++ src/tasks/services/gateways/tasks.gateway.ts
@@ -1,9 +1,9 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
-import { tasks } from "@prisma/client";
-import { Observable } from "rxjs";
 import { Socket } from 'socket.io';
-import { TasksService } from "../tasks.service";
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+
 @WebSocketGateway({
     cors: {
         origin: '*',
@@ -15,11 +15,23 @@ export class TasksGateway {
 
     constructor(
         @Inject('SOCKET_IO') private readonly socketIo: Socket,
+        @Inject(CACHE_MANAGER) private cacheManager: Cache
     ) { }
 
     @SubscribeMessage('tasks-timer')
     async emitTasksTimer(tasksIds: number[]) {
-        
+        const tasks = await this.cacheManager.get<{ id: number, currentTimerSeconds : number }[]>('tasks-timer');
+        if (!tasks || tasks.length === 0) {
+            console.log('No tasks with timer found in cache');
+            return;
+        }
+        const filteredTasks = tasks.filter(task => tasksIds.includes(task.id));
+        if (filteredTasks.length === 0) {
+            console.log('No tasks with timer found for the provided IDs');
+            return;
+        }
+        this.socketIo.emit('tasks-timer', filteredTasks);
+
         this.socketIo.emit('tasks-timer', `Timer tick: ${new Date().toISOString()}`);  
 
       
