c3140675ebab1a3b8ea0c942c16ae95a7cdb65b9
simokhounti <larbikhounti@gmail.com>
2025-08-05 10:17:47 +0100

    Add TasksGateway to providers in TasksModule

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,11 +5,12 @@ import { TasksCron } from './services/crons/tasks.cron';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ProjectsModule } from 'src/projects/projects.module';
 import { Helpers } from 'src/helpers/helper.helpers';
+import { TasksGateway } from './services/gateways/tasks.gateway';
 
 
 @Module({
   controllers: [TasksController],
-  providers: [TasksService,TasksCron, Helpers],
+  providers: [TasksService,TasksCron, Helpers, TasksGateway],
   imports: [PrismaModule, ProjectsModule],
   exports: [TasksService],
 })
